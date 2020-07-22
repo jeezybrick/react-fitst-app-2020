faacb862733f4e94a152cafdbc16fdb2878e35f2
Statsenko <smooker14@gmail.com>
2020-07-22 15:54:34 +0300

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from "react";
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">The best react app</h1>
+            <p className="lead">
+                Версия приложения <strong>1.0</strong>
+            </p>
+        </div>
+    </div>
+)
